5f50e144ee219279f9c74f6b7281fc03c3491b06
Sam Schantz <35478769+samvschantz@users.noreply.github.com>
2019-01-21 09:53:17 -0800

    Adding images and styling to navbar
    added logo and add link pic, lobster font

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
-import whale_logo from "./whale_logo.png";
+import whale_logo from "./assets/whale_logo.png";
+import plus_sign from "./assets/plus_sign.png";
 
 export const NavBar = props => {
   const mainNav = (
     <nav className="mainNav">
-      <img src={whale_logo} alt="" />
-      <p>Whalewhalewhale</p>
+      <img src={whale_logo} alt="Whale Logo" className="whaleLogo" />
+      <p className="siteName">whalewhalewhale</p>
+      <div className="addLink">
+        <img src={plus_sign} alt="Plus sign" className="plusSign" />
+        <p>add a link</p>
+      </div>
     </nav>
   );
   return <div>{mainNav}</div>;
